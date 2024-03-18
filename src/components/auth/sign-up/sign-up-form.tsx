47762d2698aff09590fbd2dc38a8ae1dbc291f46
Dorji Tshering <dorji.react@gmail.com>
2024-03-18 15:52:55 +0600

    refactor: remove unused imports

--- src/components/auth/sign-up/sign-up-form.tsx
+++ src/components/auth/sign-up/sign-up-form.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
-import Link from "next/link";
 
 const SignUpForm = () => {
   return (
